aadb63f2e8ba5b9cd14d12c66432e77c6d8f6d6f
anastasijabogavac <anastasijabogavac12@gmail.com>
2025-10-20 17:40:21 +0200

    Added comment to PrivateRoute.jsx

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import { Outlet, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+//
 const PrivateRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
 
